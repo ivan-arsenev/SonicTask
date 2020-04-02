a70735aea10faca1eef67fabd7a54ce3d628f988
ivan-arsenev <ivan_arsenev@pm.me>
2020-04-02 17:44:44 +0300

    Add backend

---
+++ backend/routes/api/profile.js
@@ -0,0 +1,88 @@
+const express = require("express");
+//const axios = require("axios");
+const config = require("config");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+// bring in normalize to give us a proper url, regardless of what user entered
+const normalize = require("normalize-url");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+// const Post = require("../../models/Post");
+
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    });
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    // only populate from user document if profile exists
+    res.json(profile.populate("user", ["name", "avatar"]));
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { status, location, bio, intrests } = req.body;
+
+    const profileFields = {
+      user: req.user.id,
+      location,
+      bio,
+      intrests: Array.isArray(intrests)
+        ? intrests
+        : intrests.split(",").map(intrest => " " + intrest.trim()),
+      status
+    };
+
+    // // Build social object and add to profileFields
+    // const socialfields = { youtube, twitter, instagram, linkedin, facebook };
+
+    // for (const [key, value] of Object.entries(socialfields)) {
+    //   if (value.length > 0)
+    //     socialfields[key] = normalize(value, { forceHttps: true });
+    // }
+    // profileFields.social = socialfields;
+
+    try {
+      // Using upsert option (creates new doc if no match is found):
+      let profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, upsert: true }
+      );
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+module.exports = router;
